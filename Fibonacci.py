44faa7103de10b421293ae90be9dc551d6f5310a
Sanya Gupta <sanyagupta2508@gmail.com>
2026-09-07 00:41:57 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n==1:
+        return 1
+    if n==0:
+        return 0
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+num=int(input("Enter number of terms"))
+for i in range(num):    
+    print(fibonacci(i),end=" ")
